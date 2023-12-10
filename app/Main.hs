a7cfba5650dfb79ef7cee68b152caba24147c484
Jake Son <sonjeabin@gmail.com>
2023-12-10 14:17:04 +0900

    pass 2nd stage

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@
 
 module Main (main) where
 
-import Network.Simple.TCP (serve, HostPreference(HostAny), closeSock)
+import Network.Simple.TCP (serve, HostPreference(HostAny), closeSock, send)
 
 
 main :: IO ()
@@ -12,9 +12,9 @@ main = do
     -- You can use print statements as follows for debugging, they'll be visible when running tests.
     putStrLn "Logs from your program will appear here"
 
-    -- Uncomment this block to pass stage 1
     let port = "6379"
     putStrLn $ "Redis server listening on port " ++ port
     serve HostAny port $ \(socket, address) -> do
         putStrLn $ "successfully connected client: " ++ show address
+        send socket "+PONG\r\n"
         closeSock socket
